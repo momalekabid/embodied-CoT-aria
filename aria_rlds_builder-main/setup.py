da7fdd563aeae0df6595b38ed2fe1837a7f0d947
momalekabid <moabid75@gmail.com>
2025-12-12 21:05:28 +0100

    tfds registration redundancy

--- aria_rlds_builder-main/setup.py
+++ aria_rlds_builder-main/setup.py
@@ -1,3 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name="aria_dataset", packages=["aria_dataset"])
+setup(
+    name="aria_dataset",
+    packages=find_packages(),
+    entry_points={
+        "tensorflow_datasets": [
+            "aria_dataset = aria_dataset:AriaDataset",
+        ],
+    },
+)
